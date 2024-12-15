7a149a5e57073c1d85f2c01eccdbf1dc87c4d78f
dax <dax26d@gmail.com>
2024-12-15 16:48:10 +0800

    完成卡片的形状 并添加了第一个按钮

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var emojis: [String] = ["😈", "👹", "👺", "🤡", "💩", "👻", "💀"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            cards
+            Button(action: {
+                // 示例功能：在点击时添加一个新卡片
+                emojis.append("🎃")
+            }) {
+                Label("Add Cards", systemImage: "plus.square.fill.on.square.fill")
+                    .labelStyle(.titleAndIcon)
+            }
+            .padding()
         }
-        .padding()
+    }
+
+    var cards: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 80, maximum: 100))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = false
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(.orange, lineWidth: 2)
+                .fill(isFaceUp ? .white : .orange)
+                .aspectRatio(2 / 3, contentMode: .fit)
+            Text(content)
+                .font(.system(size: 50))
+                .bold()
+                .opacity(isFaceUp ? 1 : 0)
+        }.onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
